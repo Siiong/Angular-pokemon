0eeeece2e14df7c1e6e53c9a4546c6cfdc067d66
bu7ch <dutchr@live.fr>
2021-06-14 13:38:59 +0200

    Adding service to app

--- src/app/components/pokemons/pokemons.component.ts
+++ src/app/components/pokemons/pokemons.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit,  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pokemon } from 'src/app/pokemon.model';
 import { PokemonsService } from 'src/app/services/pokemons.service';
 
 @Component({
   selector: 'app-pokemons',
   templateUrl: './pokemons.component.html',
-  styleUrls: ['./pokemons.component.css']
+  styleUrls: ['./pokemons.component.css'],
 })
 export class PokemonsComponent implements OnInit {
-pokemons:Pokemon[] = [];
-count = 0
+  select: any;
+  pokemons: Pokemon[] = [];
+  count = 0;
 
-  constructor(private PokeService : PokemonsService) { }
+  constructor(private PokeService: PokemonsService) {}
 
   ngOnInit(): void {
     this.pokemons = this.PokeService.getPokemons();
-    this.count = this.PokeService.comptagePokemons()
+    this.count = this.PokeService.comptagePokemons();
   }
- 
 }
